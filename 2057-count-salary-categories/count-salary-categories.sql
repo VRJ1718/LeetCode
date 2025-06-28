292403a72f9800c3150be7caad7de76a479e49c0
VRJ1718 <vrjjaiswal1718@gmail.com>
2025-06-28 22:18:11 +0530

    Time: 1512 ms (63.83%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,7 +1,7 @@
 select 'Low Salary' as category , count(*) as accounts_count
 from Accounts
 where income < 20000
-union
+union 
 select 'Average Salary' as category , count(*) as accounts_count
 from Accounts
 where income between 20000 and 50000
